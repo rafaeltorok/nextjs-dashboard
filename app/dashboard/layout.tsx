e85d6faf683626cbac11eb595eb1e6c876f20bf1
rafaeltorok <rafaeltorok@gmail.com>
2026-09-22 11:39:25 -0300

    update: add session check to protect the dashboard related routes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,15 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import SideNav from "@/app/ui/dashboard/sidenav";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  // Confirm there is an user currently logged in
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
