418491219b55973aaa852005ee66d9ac62720892
rafaeltorok <rafaeltorok@gmail.com>
2026-09-22 15:18:48 -0300

    update: add allowed dev origins config for local network devices

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allows all local devices access to the Next Dev Server
+  allowedDevOrigins: ["192.168.0.*:3000", "192.168.0.*", "0.0.0.0", "0.0.0.0:3000"],
 };
 
 export default nextConfig;
