b3244809d9d3d9b6b93e31175e2898ae38a6b81a
rafaeltorok <rafaeltorok@gmail.com>
2026-09-22 12:06:54 -0300

    update: implement a proper customers page to display the customers data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function CustomersPage() {
-  return <p>Customers page</p>;
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div>
+      <CustomersTable
+        customers={customers}
+      />
+    </div>
+  );
 }
